8c0d8da0c207229d052c9f2fe696b7e14641fa7b
h162169 <h162169@stud.u-szeged.hu>
2024-07-13 09:55:52 +0200

    profile settings design

--- resources/js/Components/ActionMessage.vue
+++ resources/js/Components/ActionMessage.vue
@@ -6,8 +6,15 @@ defineProps({
 
 <template>
     <div>
-        <transition leave-active-class="transition ease-in duration-1000" leave-from-class="opacity-100" leave-to-class="opacity-0">
-            <div v-show="on" class="text-sm text-gray-600">
+        <transition
+            leave-active-class="transition duration-1000 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div
+                v-show="on"
+                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-green-600 border border-transparent rounded-md"
+            >
                 <slot />
             </div>
         </transition>
